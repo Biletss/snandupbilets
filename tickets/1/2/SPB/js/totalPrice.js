8a4a8d37c4219b9f8a439c081f803085fee5ff73
Biletss <160591348+Biletss@users.noreply.github.com>
2024-02-21 14:17:54 +0200

    Delete tickets/1 directory

--- tickets/1/2/SPB/js/totalPrice.js
+++
@@ -1,3 +0,0 @@
-// Второй файл (где вы пытаетесь получить totalPrice)
-const totalPriceFromLocalStorage = localStorage.getItem('totalPrice');
-console.log('Total Price from localStorage:', totalPriceFromLocalStorage);
