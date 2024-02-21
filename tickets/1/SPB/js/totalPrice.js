f839eedc9a30e671e1be143e08520cb2bc3081dd
Biletss <160591348+Biletss@users.noreply.github.com>
2024-02-21 14:20:50 +0200

    Add files via upload

---
+++ tickets/1/SPB/js/totalPrice.js
@@ -0,0 +1,3 @@
+// Второй файл (где вы пытаетесь получить totalPrice)
+const totalPriceFromLocalStorage = localStorage.getItem('totalPrice');
+console.log('Total Price from localStorage:', totalPriceFromLocalStorage);
